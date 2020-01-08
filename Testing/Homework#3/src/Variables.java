52b2186a26e27c033176e09ebf1b7b272bad500f
Viktoriia <v.horditsa@gmail.com>
2020-01-08 19:50:01 +0200

    Create Variables.java

---
+++ Testing/Homework#3/src/Variables.java
@@ -0,0 +1,18 @@
+
+public class Variables {
+
+    private static int integermax = 2147483647;
+    private static int integermin = -2147483648;
+
+
+    private static byte bytemax = 127;
+    private static byte bytemin = -128;
+
+    private static short shortmax = 32767;
+    private static short shortmin = -32768;
+    
+
+    public static void main(String[] args) {
+        System.out.println(integermax );
+    }
+}
