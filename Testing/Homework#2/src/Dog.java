cc25203fa66a25134fccb1bfe0c046757d28895b
Viktoriia <v.horditsa@gmail.com>
2020-01-08 18:58:41 +0200

    Finish 2nd task

--- Testing/Homework#2/src/Dog.java
+++ Testing/Homework#2/src/Dog.java
@@ -1,13 +1,13 @@
 public class Dog extends Animal implements barking, eating {
 
     private static String name = "Muchtar";
-    private static String animalClass = "Mammals";
+    private static String animalClass = "Mammals Animal's Class";
     private static String saying = "Always I am saying like -";
 
 
     @Override
     public void barking() {
-        System.out.println("My name is" + name + "I am from" + animalClass + saying + "Wof Wof");
+        System.out.println("My name is " + name + " I am from " + animalClass + saying + " Wof Wof ");
     }
 
     @Override
@@ -15,5 +15,10 @@ public class Dog extends Animal implements barking, eating {
         System.out.println("Yummy");
     }
 
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.barking();
+        dog.eating();
+    }
 
 }
--- Testing/Homework#2/src/Sparrow.java
+++ Testing/Homework#2/src/Sparrow.java
@@ -1,11 +1,8 @@
 public class Sparrow extends Animal implements eating, flying {
 
     private static String name = "Jack";
-    private static String animalClass = "Birds";
+    private static String animalClass = "Birds Animal's Class";
     private static String mowing = "Always I am moving like -";
-
-    Sparrow sparrow = new Sparrow();
-
 
     @Override
     public void eating() {
@@ -14,6 +11,12 @@ public class Sparrow extends Animal implements eating, flying {
 
     @Override
     public void flying() {
-        System.out.println("My name is" + name + "I am from" + animalClass + mowing + "Fly Fly");
+        System.out.println("My name is " + name + " I am from " + animalClass + mowing + " Fly Fly ");
+    }
+
+    public static void main(String[] args) {
+       Sparrow sparrow = new Sparrow();
+       sparrow.eating();
+       sparrow.flying();
     }
 }
